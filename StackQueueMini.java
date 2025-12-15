7bddb0d1fb56b7e49f00f49fbae2a8316a61fcde
Chris Lee <chriswole19@gmail.com>
2025-12-15 18:19:50 +0000

    Started

--- StackQueueMini.java
+++ StackQueueMini.java
@@ -2,9 +2,16 @@ import java.util.Scanner;
 import java.util.Stack;
 import java.util.Queue;
 import java.util.LinkedList;
-
+import java.util.Scanner;
 class StackQueueMini {
   public static void main(String[] args) {
-
+    Scanner in = new Scanner(System.in);
+    Queue<String> q = new LinkedList<String>();
+    for(int i=0;i<10;i++)
+    {
+      System.out.print("Enter a number");
+      q.add(in.next());
+    }
+    scanner.close();
   } // end main
 } // end class
